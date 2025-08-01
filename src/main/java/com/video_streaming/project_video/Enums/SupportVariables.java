b127e226151a850808eab1ebfe1ed86c10e23149
Mist-AA <ayushm.agg@gmail.com>
2025-08-01 16:32:53 +0530

    Updating max upload size

--- src/main/java/com/video_streaming/project_video/Enums/SupportVariables.java
+++ src/main/java/com/video_streaming/project_video/Enums/SupportVariables.java
@@ -4,24 +4,20 @@ import java.util.List;
 
 public interface SupportVariables {
     List<String> WHITELISTED_PATHS = List.of(
-        "/api/app/health",
-        "/api/app/createUser",
-        "/api/app/login",
-        "/api/app/verifyToken",
-        "/api/app/reset",
-        "/api/vid/view/**",
-        "/api/vid/all"
-    );
+            "/api/app/**",
+            "/api/vid/view/**",
+            "/api/vid/all"
+            );
 
-   List<String> ALLOWED_ORIGINS = List.of(
-        "https://bideo.tech", 
-        "https://www.bideo.tech"
-        // Uncomment the line below for development mode
-//        ,"http://localhost:3000"
-    );
+    List<String> ALLOWED_ORIGINS = List.of(
+            "https://bideo.tech",
+            "https://www.bideo.tech"
+            // Uncomment the line below for development mode
+            // ,"http://localhost:3000"
+            );
 
     String refreshTokenURLSuffix = "https://securetoken.googleapis.com/v1/token?key=";
-    
+
     String signInTokenURLSuffix = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
 
     String resetUserURLSuffix = "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=";
@@ -29,6 +25,6 @@ public interface SupportVariables {
     String thumbnailURLDefault = "https://github.com/shadcn.png";
 
     String processedVideosFolderPath = "processed_videos";
-    
+
     int cacheTTLMinutes = 3;
 }
